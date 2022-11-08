3434184a8851270509cf0f3e935bae408c1929ad
chiyanglin-AStar <chiyanglin@gmail.com>
2022-11-08 07:16:01 +0000

    update bignum 200! example

--- big-num_ex/src/main.rs
+++ big-num_ex/src/main.rs
@@ -1,14 +1,11 @@
-fn main() {
-
-	use num_bigint::{BigInt, Sign};
-
-assert_eq!(BigInt::from_bytes_be(Sign::Plus, b"A"),
-           BigInt::parse_bytes(b"65", 10).unwrap());
-assert_eq!(BigInt::from_bytes_be(Sign::Plus, b"AA"),
-           BigInt::parse_bytes(b"16705", 10).unwrap());
-assert_eq!(BigInt::from_bytes_be(Sign::Plus, b"AB"),
-           BigInt::parse_bytes(b"16706", 10).unwrap());
-assert_eq!(BigInt::from_bytes_be(Sign::Plus, b"Hello world!"),
-           BigInt::parse_bytes(b"22405534230753963835153736737", 10).unwrap());    
+fn main() {	    
+    use num_bigint::{BigInt, Sign};    
+    
+    let mut a = BigInt::new(Sign::Plus,vec![1,0]);
+    println!("{a}");
+    for  i in 1..201{
+        a *= i;
+        println!("{i}!={a}");
+    }
 	println!("Hello, world!");
 }
