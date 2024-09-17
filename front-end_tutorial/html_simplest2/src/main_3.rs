65228d0287a6a2991957a0de3979892c60328a62
chiyanglin-AStar <chiyanglin@gmail.com>
2024-09-17 08:23:29 +0000

    update

--- front-end_tutorial/html_simplest2/src/main_3.rs
+++ front-end_tutorial/html_simplest2/src/main_3.rs
@@ -2,20 +2,17 @@
 
 use std::fs::read_to_string;
 use rocket::response::content::RawHtml;
-//use rocket::response::content;
 use std::path::PathBuf;
 
-
 #[get("/")]
-async fn index() -> Html<String> {
+async fn index() -> RawHtml<String> {
     // Path to your index.html
     let path = PathBuf::from("index.html");
-    //let response = content::RawHtml;
+    
     // Read the file content into a string
     let html_content = read_to_string(path)
         .unwrap_or_else(|_| "<h1>Error: Could not read index.html</h1>".to_string());
     
-    //response(html_content)
     RawHtml(html_content)
 }
 
