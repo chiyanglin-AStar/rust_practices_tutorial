aa6f2d4b44153e961391aeb26852cbd9ee5fddf1
chiyanglin-AStar <chiyanglin@gmail.com>
2024-10-08 13:03:29 +0000

    update

---
+++ rust_by_example/06_Conversion/from_into.rs
@@ -0,0 +1,19 @@
+use std::convert::Into;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl Into<Number> for i32 {
+    fn into(self) -> Number {
+        Number { value: self }
+    }
+}
+
+fn main() {
+    let int = 5;
+    // Try removing the type annotation
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+}
