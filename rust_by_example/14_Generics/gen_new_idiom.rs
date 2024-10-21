1819a60d9156a2430a3fa426b09c98b9067db07d
chiyanglin <chiyanglin@gmail.com>
2024-10-21 09:30:40 +0800

    Create gen_new_idiom.rs

---
+++ rust_by_example/14_Generics/gen_new_idiom.rs
@@ -0,0 +1,29 @@
+struct Years(i64);
+
+struct Days(i64);
+
+impl Years {
+    pub fn to_days(&self) -> Days {
+        Days(self.0 * 365)
+    }
+}
+
+
+impl Days {
+    /// truncates partial years
+    pub fn to_years(&self) -> Years {
+        Years(self.0 / 365)
+    }
+}
+
+fn is_adult(age: &Years) -> bool {
+    age.0 >= 18
+}
+
+fn main() {
+    let age = Years(25);
+    let age_days = age.to_days();
+    println!("Is an adult? {}", is_adult(&age));
+    println!("Is an adult? {}", is_adult(&age_days.to_years()));
+    // println!("Is an adult? {}", is_adult(&age_days));
+}
