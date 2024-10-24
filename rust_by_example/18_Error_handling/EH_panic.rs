8998629031165333a0d7755cb0f9071da4b6da05
chiyanglin <chiyanglin@gmail.com>
2024-10-24 11:39:14 +0800

    Create EH_panic.rs

---
+++ rust_by_example/18_Error_handling/EH_panic.rs
@@ -0,0 +1,12 @@
+fn drink(beverage: &str) {
+    // You shouldn't drink too much sugary beverages.
+    if beverage == "lemonade" { panic!("AAAaaaaa!!!!"); }
+
+    println!("Some refreshing {} is all I need.", beverage);
+}
+
+fn main() {
+    drink("water");
+    drink("lemonade");
+    drink("still water");
+}
