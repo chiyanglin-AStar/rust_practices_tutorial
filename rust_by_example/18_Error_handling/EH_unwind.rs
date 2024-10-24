dfa9767a3db57e86e7acfe4e5e70e521c77432a2
chiyanglin <chiyanglin@gmail.com>
2024-10-24 14:39:47 +0800

    Create EH_unwind.rs

---
+++ rust_by_example/18_Error_handling/EH_unwind.rs
@@ -0,0 +1,22 @@
+#[cfg(panic = "unwind")]
+fn ah() {
+    println!("Spit it out!!!!");
+}
+
+#[cfg(not(panic = "unwind"))]
+fn ah() {
+    println!("This is not your party. Run!!!!");
+}
+
+fn drink(beverage: &str) {
+    if beverage == "lemonade" {
+        ah();
+    } else {
+        println!("Some refreshing {} is all I need.", beverage);
+    }
+}
+
+fn main() {
+    drink("water");
+    drink("lemonade");
+}
